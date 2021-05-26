bab28ce32f502b3837deaa6f68eecad9a130b7ba
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-26 15:18:56 +0200

    Added customizable mariadb port

--- mariadb/install.sh
+++ mariadb/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-while getopts n:p: flag
+while getopts n:p:m: flag
 do
   case "${flag}" in
     n) network=${OPTARG};;
     p) pmaport=${OPTARG};;
+    m) mariaport=${OPTARG};;
   esac
 done
 
 docker build -t mariadb .
 docker network create $network
-docker run -d --network $network -p 3306:3306 --restart=always mariadb
+docker run -d --network $network -p $mariaport:3306 --restart=always mariadb
 docker run -d --network $network -p $pmaport:80 --restart=always phpmyadmin
