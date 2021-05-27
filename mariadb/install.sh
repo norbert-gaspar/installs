98012e32d5dd9e6c77d959f882dc47d3de6978c0
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-27 09:24:50 +0200

    Fixing flags

--- mariadb/install.sh
+++ mariadb/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-while getopts n:p:mp:mn: flag
+while getopts n:p:m:o: flag
 do
   case "${flag}" in
     n) network=${OPTARG};;
     p) pmaport=${OPTARG};;
-    mp) mariaport=${OPTARG};;
-    mn) marianame=${OPTARG};;
+    m) mariaport=${OPTARG};;
+    o) marianame=${OPTARG};;
   esac
 done
 
@@ -13,3 +13,4 @@ docker build -t mariadb .
 docker network create $network
 docker run -d --network $network -p $mariaport:3306 --name $marianame --restart=always mariadb
 docker run -d --network $network -p $pmaport:80 -e PMA_HOST=$marianame -e PMA_USER=myMariadbUser -e PMA_PASSWORD=myMariadbPassword --restart=always phpmyadmin
+You have new mail in /var/mail/pi
