e677290729cc116a4e4d9c831d0049fd31e6dc30
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-27 09:16:25 +0200

    Tweaking syntax

--- mariadb/install.sh
+++ mariadb/install.sh
@@ -11,5 +11,5 @@ done
 
 docker build -t mariadb .
 docker network create $network
-docker run -d --network $network -p $mariaport:3306 --name=$marianame --restart=always mariadb
-docker run -d --network $network -p $pmaport:80 -e PMA_HOST=$marianame -e PMA_USER=myMariadbUser -e PMA_PASSWORD=myMariadbPassword --restart=always phpmyadmin
+docker run -d --network $network -p $mariaport:3306 --name $marianame --restart always mariadb
+docker run -d --network $network -p $pmaport:80 -e PMA_HOST=$marianame -e PMA_USER=myMariadbUser -e PMA_PASSWORD=myMariadbPassword --restart always phpmyadmin
