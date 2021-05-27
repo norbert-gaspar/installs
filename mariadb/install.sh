5da22eaff9899652cc296287e77f4e4d6bdfaf05
norbert-gaspar <68104997+norbert-gaspar@users.noreply.github.com>
2021-05-27 09:08:05 +0200

    Hardcoding credentials

--- mariadb/install.sh
+++ mariadb/install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-while getopts n:p:m: flag
+while getopts n:p:mp:mn: flag
 do
   case "${flag}" in
     n) network=${OPTARG};;
     p) pmaport=${OPTARG};;
-    m) mariaport=${OPTARG};;
+    mp) mariaport=${OPTARG};;
+    mn) marianame=${OPTARG};;
   esac
 done
 
 docker build -t mariadb .
 docker network create $network
-docker run -d --network $network -p $mariaport:3306 --restart=always mariadb
-docker run -d --network $network -p $pmaport:80 --restart=always phpmyadmin
+docker run -d --network $network -p $mariaport:3306 --name $marianame --restart=always mariadb
+docker run -d --network $network -p $pmaport:80 -e PMA_HOST=$marianame -e PMA_USER=myMariadbUser -e PMA_PASSWORD=myMariadbPassword --restart=always phpmyadmin
